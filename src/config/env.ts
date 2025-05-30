743aa18c773124cc588aa6a17a3386cbaa579d53
Joseph Ng <joseph_sieang@live.com>
2025-05-30 11:29:52 +0800

    Load bearer token with config

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,3 +3,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const GREETING_NAME = process.env.GREETING_NAME || 'world';
+export const BEARER_TOKEN = process.env.BEARER_TOKEN || '';
