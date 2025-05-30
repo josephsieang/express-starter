d41ba08d8d683a51618a8d3d85536d90485a7995
Joseph Ng <joseph_sieang@live.com>
2025-05-30 10:49:55 +0800

    Add logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import helloRoute from './routes/hello.route';
+import { logger } from './middlewares/logger.middleware';
 
 const app = express();
 
 app.use(express.json());
 app.use('/hello', helloRoute);
+app.use(logger);
 
 export default app;
