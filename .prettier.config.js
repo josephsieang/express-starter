c06570f70113bc751d95d6ba55903d2eb435fdb1
Joseph Ng <joseph_sieang@live.com>
2025-05-30 10:36:55 +0800

    Use single quote

--- .prettier.config.js
+++ .prettier.config.js
@@ -1,8 +1,8 @@
 // .prettier.config.js
 module.exports = {
   semi: true,
-  singleQuote: false,
+  singleQuote: true,
   printWidth: 100,
   tabWidth: 2,
-  trailingComma: 'es5'
+  trailingComma: "es5",
 };
