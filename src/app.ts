269e4786aebfdbef4ed460c4d72759e75fed1f09
Joseph Ng <joseph_sieang@live.com>
2025-05-30 11:08:15 +0800

    Handle error

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import helloRoute from './routes/hello.route';
 import { logger } from './middlewares/logger.middleware';
+import { errorHandler } from './middlewares/error.middleware';
+import testRoute from './routes/test.route';
 
 const app = express();
 
 app.use(express.json());
-app.use('/hello', helloRoute);
 app.use(logger);
+app.use('/hello', helloRoute);
+app.use('/test', testRoute);
+app.use(errorHandler); // should be last
 
 export default app;
