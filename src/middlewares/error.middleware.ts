6e6307b8d20531f8f94b618c6036eb61fe3dd331
Joseph Ng <joseph_sieang@live.com>
2025-05-31 10:48:38 +0800

    Add error logger for request

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,8 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  console.error('request error:', {
+    method: req.method,
+    url: req.originalUrl,
+    body: req.body,
+    params: req.params,
+    query: req.query
+  });
   console.error('❌ Error:', err);
   res.status(err.status || 500).json({
-    error: err.message || 'Internal Server Error',
+    error: err.message || 'Internal Server Error'
   });
 }
